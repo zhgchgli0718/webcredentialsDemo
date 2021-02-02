e35d32f6c8748c004b0afea4129e70177c86edd1
harris <harris@streetvoice.com>
2021-02-02 10:14:31 +0800

    Initial Commit

---
+++ webcredentialsDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  webcredentialsDemo
+//
+//  Created by Harris Li on 2021/2/2.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
